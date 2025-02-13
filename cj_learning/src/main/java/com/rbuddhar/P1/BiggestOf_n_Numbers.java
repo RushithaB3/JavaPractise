2f9be287571f934e282f0568310992e35530ca1c
rbuddhar <rushitha.ed@gamil.com>
2025-02-13 10:00:42 -0600

    Solved assignment given on if condition

--- cj_learning/src/main/java/com/rbuddhar/P1/BiggestOf_n_Numbers.java
+++ cj_learning/src/main/java/com/rbuddhar/P1/BiggestOf_n_Numbers.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 
 public class BiggestOf_n_Numbers {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter how many numbers you would like to enter: ");
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        int i ;
+        int max = arr[0];
+        System.out.println("Enter elements of array: ");
+        for (i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        max = arr[0];
+        for (i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
 
-
-
-
-
-
+        System.out.println("Biggest is :"+max);
     }
 }
